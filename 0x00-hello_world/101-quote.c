b1c4161ffea30558ef535ddfb8d258862d7d0805
Derick Pambah <pambahderick412@gmail.com>
2021-06-17 13:52:41 -0700

    Put

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,15 @@
 #include <unistd.h>
 
 /**
- *  *main - Entry point
+ *  * main - Entry point
  *   *
- *    *Return: returns 1 upon successful compilation
- *     */
+ *    * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *     *
+ *      * Return: Always 0 (Success)
+ *       */
 
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(2, str1, 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
+
 #include <stdio.h>
+/**
+ *main - use printf typeof
+ *Description: output with printf
+ *Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
