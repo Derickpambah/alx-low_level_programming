1b6ff4a1ffd5e2398cbdf595d1fae1a604d6e70e
Derick Pambah <pambahderick412@gmail.com>
2021-06-18 14:49:51 -0700

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,16 +13,14 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a';c <= 'z';)
+	for (c = 97 ; c < 123 ; c++)
 	{
 		putchar(c);
-		c = c + 1;
 	}
-	for (c = 'A';c <= 'Z';)
+	for (c = 65 ; c < 91 ; c++)
 	{
 		putchar(c);
-		c = c + 1;
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
