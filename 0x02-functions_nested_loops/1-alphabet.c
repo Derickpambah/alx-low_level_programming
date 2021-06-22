3bf2c29ca576c7acf7dab08ef5d66e3740a90df3
Derick Pambah <pambahderick412@gmail.com>
2021-06-22 12:22:02 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	  char ch;
+
+	  ch = 'a';
+
+	  while (ch <= 'z')
+	  {
+		  _putchar(ch);
+		  ch++;
+	  }
+	  _putchar('\n');
+}
