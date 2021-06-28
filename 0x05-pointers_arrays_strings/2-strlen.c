fb79501d70b8699c39e2aa7d3ac5d9501778eea0
Derick Pambah <pambahderick412@gmail.com>
2021-06-28 13:50:33 -0700

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,12 +2,12 @@
 #include "holberton.h"
 
 /**
- *  * _strlen - checks the length of a string
- *   * @s: character pointer
- *    *
- *     * Return: length of a string
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
+ * _strlen - checks the length of a string
+ * @s: character pointer
+ *
+ * Return: length of a string
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 int _strlen(char *s)
 {
