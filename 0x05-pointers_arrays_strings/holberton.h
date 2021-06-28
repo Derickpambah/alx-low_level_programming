c7397e9680d8e95fc0878839258424d468443473
Derick Pambah <pambahderick412@gmail.com>
2021-06-28 13:46:44 -0700

    Holberton file

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,10 +1,8 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
@@ -12,4 +10,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-#endif /* HOLBERTON_H */
