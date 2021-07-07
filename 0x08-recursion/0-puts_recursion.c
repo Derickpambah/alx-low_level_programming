f6100a33b62a1258d90e8bc8aef60e9d8f0ab1d3
Derick Pambah <pambahderick412@gmail.com>
2021-07-07 11:51:36 -0700

    Recursion - puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * Write a function that prints a string, followed by a new line
+ */
+
+int main()
+{
+  int s;
+
+  if (s = " ")
+    {
+      return 0;
+    }
+  
+  _putchar("s");
+  _puts_recursion(s + 1);
+    
+}
