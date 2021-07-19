01fccf80a0b451dc94f8af7ffaec76d16a7790c9
Derick Pambah <pambahderick412@gmail.com>
2021-07-19 12:05:40 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#ifndef __FILE__
-#define __FILE__
-#endif
+
 /**
- * main - print the name of the file
- *Return: 0 (success)
+ * main - prints the name of the file
+ *
+ * Return: always (0)
  */
 int main(void)
 {
   printf("%s\n", __FILE__);
+
   return (0);
+
 }
