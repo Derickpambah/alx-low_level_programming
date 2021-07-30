3bce6974a8b9ef139ec208538ca8cd2fdafa3090
Derick Pambah <pambahderick412@gmail.com>
2021-07-29 19:27:53 -0700

    Add node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,46 +1,50 @@
 #include "lists.h"
+
 /**
- * add_node_end - adds a node at the end of a list
- * @head: pointer to the first element of the list
- * @str: pointer to a string to be stored
- * Return: pointer to the first element of the list
+ * _strlen - find string length
+ * @str: string
+ * Return: length
+ */
+int _strlen(const char *str)
+{
+  int len;
+
+  for (len = 0; str[len] != '\0'; len++)
+    ;
+  return (len);
+}
+/**
+ * add_node_end - add node to end of linked list
+ * @head: linked list
+ * @str: data for new node
+ * Return: address of new element, or NULL if failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-  list_t *temp, *new;
+  list_t *new_node, *tmp; /* create new node */
 
-  temp = *head;
-  new = malloc(sizeof(list_t));
-  if (new == NULL)
+  if (str == NULL) /* validate input */
     return (NULL);
-  new->str = strdup(str);
-  new->len = _strlen(str);
-  new->next = NULL;
+  if (strdup(str) == NULL) /* check if malloc errored */
+    return (NULL);
 
-  if (*head == NULL)
-    {
-      *head = new;
-      return (*head);
-    }
-  while (temp->next)
-    {
-      temp = temp->next;
-    }
-  temp->next = new;
-  return (*head);
-}
-/**
- * _strlen - determinates the length of a string
- * @s: pointer to string
- * Return: the length
- */
-int _strlen(const char *s)
-{
-  int a;
-  int len;
-  for (a = 0; s[a] != '\0'; a++)
-    {
-      len++
-    }
-  return (a);
+  new_node = malloc(sizeof(list_t)); /* malloc for new node */
+  if (new_node == NULL)
+    return (NULL);
+
+  new_node->str = strdup(str); /* set node values */
+  new_node->len = _strlen(str);
+  new_node->next = NULL;
+
+  if (*head == NULL) /* if no list nodes, set new_node to beginning */
+    *head = new_node;
+  else
+  {
+    tmp = *head;
+
+    while (tmp->next != NULL)
+      tmp = tmp->next;
+    tmp->next = new_node;
+  }
+  return (new_node);
 }
